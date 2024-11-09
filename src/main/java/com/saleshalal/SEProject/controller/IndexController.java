a17417074f1f1f8de856f7047555c90492850839
Omar Elmady <oelmady@gmail.com>
2024-11-09 10:09:01 -0500

    updated indexing

--- src/main/java/com/saleshalal/SEProject/controller/IndexController.java
+++ src/main/java/com/saleshalal/SEProject/controller/IndexController.java
@@ -1,7 +1,5 @@
 package com.saleshalal.SEProject.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
